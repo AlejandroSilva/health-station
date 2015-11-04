cb57bbbacfb8d517215fdc9189360d1581504eeb
AlejandroSilva <pm5k.sk@gmail.com>
2015-11-04 13:31:56 -0300

    network module fail on test

--- modules/Network.js
+++ modules/Network.js
@@ -103,7 +103,6 @@ export const interfacesInfo = ()=>{
             let promiseEnd = _getInterfacesInfo()
             Promise.all([promiseStart, promiseEnd])
                 .then(([start, end])=>{
-                    console.log(start, end)
                     // calcular la diferencia de datos en el tiempo
                     resolve(
                         end.map((int, index)=>{
